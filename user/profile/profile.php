cfbc515ce40fbd1181414258fb0bdea3220cc1d2
codecpacka <ramprasad.r2600@gmail.com>
2021-03-29 18:50:42 +0530

    made profile page

---
+++ user/profile/profile.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>
+        profile
+    </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/custom.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="dashboard.css">
+</head>
+<style>
+    .container-sm {
+        /* background-color: blue; */
+        /* max-width: 110rem; */
+        /* position: relative; */
+
+    }
+
+    .profilebg {
+        background-image: url(dish.png);
+        position: relative;
+        height: 200px;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        /* background-position: 10% 65%; */
+        /* position: absolute; */
+    }
+
+    .profileinfo {
+        /* position: absolute; */
+        position: absolute;
+        margin-top: 18px;
+        margin-left: 7px;
+    }
+
+    .profileedit {
+        position: absolute;
+        margin-top: 57px;
+        margin-left: 1117px;
+    }
+
+    #profileimg {
+        border: solid white 2px;
+        border-radius: 50%;
+        height: 174px;
+        width: auto;
+    }
+
+    .cwhite {
+        color: white;
+
+    }
+
+    .edit {
+        background-color: rgb(237, 90, 107);
+        padding: 11px;
+        color: white;
+        border-radius: 10px;
+        border: none;
+
+    }
+</style>
+
+<body>
+    <div class="container-sm" style="margin: 0px 146px 0px 146px;">
+        <div class="profilebg">
+            <div class="profileinfo">
+                <img id="profileimg" src="profile.png" alt="profile image" style="float: left;">
+                <div style="margin-top:45px; margin-left:16px; float:right;">
+                    <div class="cwhite" style="font-weight:bolder; font-size:22px;">Ramprasad sarkar</div>
+                    <div class="cwhite">ramprasad.r2600@gmail.com</div>
+                    <div class="cwhite">bhavnagar</div>
+                </div>
+
+            </div>
+            <div class="profileedit">
+                <button class="edit" role="button" tabindex="0" aria-disabled="false"><span tabindex="-1" class="sc-1kx5g6g-2 gylDKm"><i class="rbbb40-1 MxLSp sc-1kx5g6g-0 gyojlT" color="#FFFFFF" size="10"><svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" width="10" height="10" viewBox="0 0 20 20" aria-labelledby="icon-svg-title- icon-svg-desc-" role="img" class="rbbb40-0 fdFvgA">
+                                <path d="M16.66 11.9c-0.522 0.011-0.94 0.437-0.94 0.96 0 0 0 0 0 0v-0 3.32c0 0.006 0 0.013 0 0.020 0 1.049-0.851 1.9-1.9 1.9-0 0-0 0-0 0h-10c-0.006 0-0.013 0-0.020 0-1.049 0-1.9-0.851-1.9-1.9 0-0.007 0-0.014 0-0.021v0.001-10c0-0 0-0 0-0 0-1.049 0.851-1.9 1.9-1.9 0.007 0 0.014 0 0.021 0h3.319c0.467-0.070 0.822-0.469 0.822-0.95s-0.354-0.88-0.817-0.949l-0.005-0.001h-3.32c-0 0-0 0-0 0-2.103 0-3.809 1.699-3.82 3.799v10.001c0 2.11 1.71 3.82 3.82 3.82v0h10c2.101-0.011 3.8-1.717 3.8-3.82 0-0 0-0 0-0v0-3.32c0-0.53-0.43-0.96-0.96-0.96v0zM18.96 1.040c-0.648-0.647-1.542-1.047-2.53-1.047s-1.882 0.4-2.53 1.047l0-0-6.9 6.92c-0.072 0.074-0.132 0.16-0.178 0.254l-0.003 0.006v0.080s0 0 0 0.060l-1.54 5.18c-0.027 0.084-0.042 0.18-0.042 0.28 0 0.265 0.108 0.506 0.282 0.68l0 0c0.174 0.173 0.413 0.28 0.678 0.28 0.001 0 0.002 0 0.002 0h-0c0.039 0.006 0.084 0.009 0.13 0.009s0.091-0.003 0.135-0.009l-0.005 0.001 5.18-1.5h0.14c0.1-0.048 0.186-0.108 0.26-0.18l-0 0 6.92-7c0.647-0.648 1.047-1.542 1.047-2.53s-0.4-1.882-1.047-2.53l0 0zM7.62 12.38l0.52-1.9 1.38 1.38zM17.62 4.76l-6.2 6.22-2.4-2.36 6.22-6.24c0.305-0.305 0.725-0.493 1.19-0.493 0.929 0 1.683 0.753 1.683 1.683 0 0.465-0.188 0.885-0.493 1.19v0z"></path>
+                            </svg></i><span class="sc-1kx5g6g-3 dkwpEa">Edit profile</span></span></button>
+            </div>
+        </div>
+    </div>
+</body>
+<script>
+
+</script>
+
+</html>
