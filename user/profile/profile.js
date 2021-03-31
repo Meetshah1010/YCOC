694bdff01e484e3daa8c231cc4d71a0eaf9c29ab
codecpacka <ramprasad.r2600@gmail.com>
2021-03-31 22:58:17 +0530

    left panel working fine completley

--- user/profile/profile.js
+++ user/profile/profile.js
@@ -1,3 +1,43 @@
-const box = document.getElementsByClassName("stylebox");
-box.style.color = "red";
-// console.log(box);
+const box = document.querySelector(".stylebox");
+
+
+// box.style.transform="translateY(100%)";
+function getclassname(classname) {
+    console.log(classname);
+
+    function position(classname) {
+        switch (classname) {
+            case 'a':
+                return 14;
+                break;
+            case 'rb':
+                return 130;
+                break;
+            case 'fc':
+                return 242;
+                break;
+            case 'dfc':
+                return 360;
+                break;
+            case 'yf':
+                return 480;
+                break;
+            case 'oh':
+                return 598;
+                break;
+            case 'ru':
+                return 718;
+                break;
+
+        }
+    }
+    val = position(classname);
+    val = String(val + '%');
+    box.style.transform = `translateY(` + val + `)`;
+
+
+
+}
+var p = document.getElementsByClassName("rb");
+console.log(p);
+p.style.color = "red";
