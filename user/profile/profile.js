f9c2327cfc95d9087e7f990bbe29a8556f98450d
codecpacka <ramprasad.r2600@gmail.com>
2021-04-01 10:30:10 +0530

    left side animation completed and fully working

--- user/profile/profile.js
+++ user/profile/profile.js
@@ -1,9 +1,15 @@
 const box = document.querySelector(".stylebox");
+var globaloldclass = 'a';
 
 
 // box.style.transform="translateY(100%)";
 function getclassname(classname) {
     console.log(classname);
+    // var t = document.querySelector('.' + classname);
+    // t.addEventListener('click', function () {
+    //     t.style.color = "red";
+    // });
+
 
     function position(classname) {
         switch (classname) {
@@ -31,13 +37,24 @@ function getclassname(classname) {
 
         }
     }
+
     val = position(classname);
     val = String(val + '%');
     box.style.transform = `translateY(` + val + `)`;
+    // change color function
+    function changetextcolor(oldclass, classname) {
+        document.querySelector('.' + oldclass).removeAttribute("style");
+        document.querySelector('.' + classname).setAttribute("style", "color:red");
+        globaloldclass = classname;
+        console.log("after clling function");
+        console.log("oldclass " + globaloldclass);
+        console.log("newclass " + classname);
+    }
+    console.log("before function calling");
+    console.log("oldclass " + globaloldclass);
+    console.log("newclass " + classname);
+    changetextcolor(globaloldclass, classname);
 
 
 
 }
-var p = document.getElementsByClassName("rb");
-console.log(p);
-p.style.color = "red";
